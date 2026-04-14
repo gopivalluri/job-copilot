b7c3d24d3b3630be113417b0827547a307a63b2c
gopivalluri <vallurigopi1999@gmail.com>
2026-04-14 12:56:03 -0500

    Use Vercel proxy to bypass CORS

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000'}/:path*`,
+        destination: `https://radiant-nourishment-production-a4b1.up.railway.app/:path*`,
       },
     ]
   },
 }
-
 module.exports = nextConfig
